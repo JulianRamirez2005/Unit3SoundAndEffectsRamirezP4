ba21ab7b1ad37af0dd9d1fcc5d3aaf108b3a9ef1
JulianRamirez2005 <solokiller2005@gmail.com>
2023-04-05 21:37:33 -0700

    done
    done

---
+++ Unit3SoundAndEffect/Assets/Script/SpawnManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    public float startDelay = 2;
+    public float repeatRate = 2;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomObstacle", startDelay, repeatRate);
+        playerControllerScript= GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void SpawnRandomObstacle()
+    {
+        if (playerControllerScript.gameOver == false) 
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+            Vector3 SpawnManager = spawnPos;
+
+            Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
+        }
+    }
+}
